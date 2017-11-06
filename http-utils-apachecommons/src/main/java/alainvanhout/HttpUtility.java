452350a8fffcc01c5eefa1e59ec851dbb7a4a2d1
Alain Van Hout <alainvanhout@gmail.com>
2017-11-06 19:15:44 +0100

    HttpUtility class

---
+++ http-utils-apachecommons/src/main/java/alainvanhout/HttpUtility.java
@@ -0,0 +1,11 @@
+package alainvanhout;
+
+import alainvanhout.http.dtos.Request;
+import alainvanhout.http.dtos.Response;
+
+public class HttpUtility {
+
+    public Response execute(final Request request){
+        return null;
+    }
+}
