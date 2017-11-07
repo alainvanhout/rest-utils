ee1221827f69383e94d5a494f04750f52a0b6703
Alain Van Hout <alainvanhout@gmail.com>
2017-11-07 21:31:33 +0100

    Add parameters and headers fields to Request, for later use

--- http-utils/src/main/java/alainvanhout/http/dtos/Request.java
+++ http-utils/src/main/java/alainvanhout/http/dtos/Request.java
@@ -1,9 +1,13 @@
 package alainvanhout.http.dtos;
 
+import alainvanhout.http.parameters.Parameters;
+
 public class Request {
     private String url;
     private String method;
     private String body;
+    private Parameters parameters;
+    private Parameters headers;
 
     public String getUrl() {
         return url;
@@ -31,6 +35,35 @@ public class Request {
         this.body = body;
         return this;
     }
+
+    public Parameters getParameters() {
+        return parameters;
+    }
+
+    public Request parameters(final Parameters parameters) {
+        this.parameters = parameters;
+        return this;
+    }
+
+    public Request addParameters(final String key, final String... values) {
+        this.parameters.add(key, values);
+        return this;
+    }
+
+    public Parameters getHeaders() {
+        return headers;
+    }
+
+    public Request headers(final Parameters headers) {
+        this.headers = headers;
+        return this;
+    }
+
+    public Request addHeaders(final String key, final String... values) {
+        this.headers.add(key, values);
+        return this;
+    }
+
 
     @Override
     public String toString() {
