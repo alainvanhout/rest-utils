51fa46916bbb41358d5b4ee556a082198fdcd8af
Alain Van Hout <alainvanhout@gmail.com>
2017-11-12 17:18:01 +0100

    Added javadoc

--- json-utils/src/main/java/alainvanhout/json/JsonConverter.java
+++ json-utils/src/main/java/alainvanhout/json/JsonConverter.java
@@ -2,11 +2,41 @@ package alainvanhout.json;
 
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Map;
 
+/**
+ * Interface for converting to and from JSON, with a number of convenience methods
+ */
 public interface JsonConverter {
+    /**
+     * Converts an object to the equivalent JSON representation.
+     * @param object The object to be converted
+     * @return The object's JSON representation
+     */
     String toJson(final Object object);
 
+    /**
+     * Produces an object based on the provided JSON string and a {@link Class} reference.
+     * @param json The JSON string
+     * @param clazz The class of object that is to be produced
+     * @param <T> The generic associated with the class of the object that is to be produced
+     * @return The object that is to be produced
+     */
     <T> T toObject(final String json, final Class<T> clazz);
 
+    /**
+     * Produces a list based on the provided JSON string and a {@link Type} reference.
+     * @param json The JSON string
+     * @param type The Generic {@link Type} of the list that is to be produced
+     * @param <T> The generic associated with the type of the list that is to be produced
+     * @return The list that is to be produced
+     */
     <T> List<T> toList(final String json, final Type type);
+
+    /**
+     * Convenience method to produce a {@link Map} instance based on the provided JSON string
+     * @param json The JSON string
+     * @return The Map instance that is to be produced
+     */
+    Map toMap(final String json);
 }
--- json-utils/src/main/java/alainvanhout/json/JsonConverterBuilder.java
+++ json-utils/src/main/java/alainvanhout/json/JsonConverterBuilder.java
@@ -1,5 +1,22 @@
 package alainvanhout.json;
 
+/**
+ * An interface for building {@link JsonConverter} instances.
+ *
+ * Currently it this includes few configuration setters, but more will be added in the future.
+ */
 public interface JsonConverterBuilder {
+
+    /**
+     * Returns a functional {@link JsonConverter} instance based on the previously provided configuration,
+     * or the defaults for those configuration options.
+     * @return A functional {@link JsonConverter}
+     */
     JsonConverter init();
+
+    /**
+     * Configuration option, to specify that a converted should output multi-line JSON.
+     * @return The builder itself
+     */
+    JsonConverterBuilder formatted();
 }
