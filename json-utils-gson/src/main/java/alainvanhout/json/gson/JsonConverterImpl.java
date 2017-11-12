556e868da6e5ae6da85954e8a6ffe0cf20f7891f
Alain Van Hout <alainvanhout@gmail.com>
2017-11-12 17:18:38 +0100

    Added configuration option for multi-line json output, and added javadoc

--- json-utils-gson/src/main/java/alainvanhout/json/gson/JsonConverterImpl.java
+++ json-utils-gson/src/main/java/alainvanhout/json/gson/JsonConverterImpl.java
@@ -6,37 +6,68 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JsonConverterImpl implements JsonConverter, JsonConverterBuilder {
 
     private Gson gson = null;
 
+    // configuration
+    private boolean configFormatted = false;
+
+    /**
+     * Use {JsonConverterImpl::with} to create new {@link JsonConverterBuilder} instances
+     */
     private JsonConverterImpl(){
     }
 
+    /**
+     * Produces a new {@link JsonConverterBuilder} instance, to which configuration can be applied,
+     * before calling {@link JsonConverterBuilder::init} to create a {@link JsonConverter} instance
+     * @return A new {@link JsonConverterBuilder} instance
+     */
     public static JsonConverterBuilder with(){
         return new JsonConverterImpl();
     }
 
     @Override
-    public JsonConverter init() {
-        gson = new GsonBuilder().create();
+    public JsonConverterBuilder formatted() {
+        this.configFormatted = true;
         return this;
     }
 
     @Override
-    public String toJson(Object object) {
+    public JsonConverter init() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+
+        if (configFormatted){
+            gsonBuilder.setPrettyPrinting();
+        }
+
+        gson = gsonBuilder.create();
+
+        return this;
+    }
+
+    @Override
+    public String toJson(final Object object) {
         return gson.toJson(object);
     }
 
     @Override
-    public <T> T toObject(String json, Class<T> clazz) {
+    public <T> T toObject(final String json, Class<T> clazz) {
         return gson.fromJson(json, clazz);
     }
 
     @Override
-    public <T> List<T> toList(String json, Type type) {
+    public <T> List<T> toList(final String json, final Type type) {
         return gson.fromJson(json, type);
+    }
+
+    @Override
+    public Map toMap(final String json) {
+        return gson.fromJson(json, HashMap.class);
     }
 }
--- json-utils-gson/src/main/java/alainvanhout/json/gson/JsonUtility.java
+++ json-utils-gson/src/main/java/alainvanhout/json/gson/JsonUtility.java
@@ -2,6 +2,12 @@ package alainvanhout.json.gson;
 
 import alainvanhout.json.JsonConverter;
 
+/**
+ * Convenience class that provides varying typical {@link JsonConverter} instances for common use-cases.
+ */
 public class JsonUtility {
+    /**
+     * A {@link JsonConverter} instance that relies for all configuration relies on the defaults.
+     */
     public static final JsonConverter DEFAULT = JsonConverterImpl.with().init();
 }
