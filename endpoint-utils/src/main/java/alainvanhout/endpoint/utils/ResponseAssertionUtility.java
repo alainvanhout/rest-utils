2fce87646c0c4d410d7cbad974d670c004ade7c6
Alain Van Hout <alainvanhout@gmail.com>
2017-11-14 19:27:00 +0100

    Some more assertion utility methods for endpoint-utils

--- endpoint-utils/src/main/java/alainvanhout/endpoint/utils/ResponseAssertionUtility.java
+++ endpoint-utils/src/main/java/alainvanhout/endpoint/utils/ResponseAssertionUtility.java
@@ -1,7 +1,7 @@
 package alainvanhout.endpoint.utils;
 
 import alainvanhout.http.dtos.Response;
-import org.junit.Assert;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.function.Consumer;
 
@@ -9,7 +9,20 @@ import static org.junit.Assert.*;
 
 public class ResponseAssertionUtility {
 
+    public static final String INCORRECT_STATUS_CODE = "Status code did not match expected value";
+
     public static Consumer<Response> matches(int statusCode) {
-        return response -> assertEquals(statusCode, response.getStatusCode());
+        return response -> assertEquals(INCORRECT_STATUS_CODE, statusCode, response.getStatusCode());
+    }
+
+    public static Consumer<Response> matchesEmpty(int statusCode) {
+        return response -> {
+            assertEquals(INCORRECT_STATUS_CODE, statusCode, response.getStatusCode());
+            assertTrue("Response body was not empty", StringUtils.isEmpty(response.getBody()));
+        };
+    }
+
+    public static Consumer<Response> unwantedSuccess() {
+        return response -> fail("Unwanted success: " + response);
     }
 }
