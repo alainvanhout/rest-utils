a61b12e0dde0c9f39caa2ecf4e8e0f289ca61540
Alain Van Hout <alainvanhout@gmail.com>
2018-01-30 20:51:53 +0100

    Add convenience static method to JsonUtility which delegate to the DEFAULT JsonConverter

--- json-utils-gson/src/main/java/alainvanhout/json/gson/JsonUtility.java
+++ json-utils-gson/src/main/java/alainvanhout/json/gson/JsonUtility.java
@@ -2,6 +2,10 @@ package alainvanhout.json.gson;
 
 import alainvanhout.json.JsonConverter;
 
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Convenience class that provides varying typical {@link JsonConverter} instances for common use-cases.
  */
@@ -10,4 +14,25 @@ public class JsonUtility {
      * A {@link JsonConverter} instance that relies for all configuration relies on the defaults.
      */
     public static final JsonConverter DEFAULT = JsonConverterImpl.with().init();
+
+    public static String toJson(final Object object) {
+        return DEFAULT.toJson(object);
+    }
+
+    public static <T> T toObject(final String json, Class<T> clazz) {
+        return DEFAULT.toObject(json, clazz);
+    }
+
+    public static <T> T toObject(final String json, final Type type) {
+        return DEFAULT.toObject(json, type);
+    }
+
+    public static <T> List<T> toList(final String json, final Type type) {
+        return DEFAULT.toList(json, type);
+    }
+
+    public static Map toMap(final String json) {
+        return DEFAULT.toMap(json);
+    }
+
 }
